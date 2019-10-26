1a473a670c3afc37b9991b33df65a2dbf1b40a7c
kevinbarta <kevin005005@gmail.com>
2019-10-27 00:57:46 +0200

    Edit: now actualy created and added the core project files.

---
+++ CSHARP/OENIK_PROG3_2019_1_GO10DV/Languages.Data/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Languages.Data
+{
+    public class Class1
+    {
+    }
+}
---
+++ CSHARP/OENIK_PROG3_2019_1_GO10DV/OENIK_PROG3_2019_1_GO10DV/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OENIK_PROG3_2019_1_GO10DV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
