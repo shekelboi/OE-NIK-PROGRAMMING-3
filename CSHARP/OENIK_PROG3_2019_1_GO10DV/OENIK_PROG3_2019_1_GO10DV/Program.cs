c5aff013f7d7cc39b08270979438b6557d29aee2
kevinbarta <kevin005005@gmail.com>
2019-10-26 16:36:06 +0200

    Fixed the oenik.ruleset file

--- CSHARP/OENIK_PROG3_2019_1_GO10DV/OENIK_PROG3_2019_1_GO10DV/Program.cs
+++ CSHARP/OENIK_PROG3_2019_1_GO10DV/OENIK_PROG3_2019_1_GO10DV/Program.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OENIK_PROG3_2019_1_GO10DV
+﻿namespace OENIK_PROG3_2019_1_GO10DV
 {
-    class Program
+    public class Program // Main class
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
         }
     }
