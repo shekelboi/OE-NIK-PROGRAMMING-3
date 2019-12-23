e8768dcac8868920b09881c4e24b688ed2fb4b23
kevinbarta <kevin005005@gmail.com>
2019-12-23 02:03:03 +0100

    Queries added.

--- CSHARP/OENIK_PROG3_2019_2_GO10DV/Languages.Logic/Queries.cs
+++ CSHARP/OENIK_PROG3_2019_2_GO10DV/Languages.Logic/Queries.cs
@@ -9,6 +9,138 @@ namespace Languages.Logic
 {
     public static class Queries
     {
+        private static DatabaseEntities db = new DatabaseEntities();
+
+        /// <summary>
+        /// Listing the elements.
+        /// </summary>
+        public static void NamesOfCountries()
+        {
+            var q = db.Database.SqlQuery<Items<string, int>>("select name as [A], population as [B] from country;");
+
+            foreach (var item in q)
+            {
+                Console.WriteLine(item.A);
+            }
+        }
+
+        /// <summary>
+        /// Number of speakers based on the language or the difficulty.
+        /// </summary>
+        public static void NumberOfSpeakersRollup()
+        {
+            string sql = "select language.name as [A], language.difficulty as [B], sum(convert(bigint, language.number_of_speakers))" +
+                "as [C] from language group by rollup(language.difficulty, language.name);";
+            var q = db.Database.SqlQuery<Items<string, string, long>>(sql);
+
+            string header = string.Format("{0,20} {1,10} {2,20}", "Language", "Difficulty", "No. of speakers");
+
+            Console.WriteLine(header + "\n");
+
+            foreach (var item in q)
+            {
+                string s = string.Format("{0,20} {1,10} {2,20}", item.A, item.B, item.C);
+
+                Console.WriteLine(s);
+            }
+        }
+
+        /// <summary>
+        /// Selecting languages and their corresponding language families.
+        /// </summary>
+        public static void LanguageFamilies()
+        {
+            string sql = "select language.name as [A], language_family.name as [B] " +
+                "from language join langfam_lang_link on language.id = langfam_lang_link.lang_id " +
+                "join language_family on language_family.id = langfam_lang_link.langfam_id;";
+            var q = db.Database.SqlQuery<Items<string, string>>(sql);
+
+            string header = string.Format("{0,20} {1,30}", "Language", "Language family");
+
+            Console.WriteLine(header + "\n");
+
+            foreach (var item in q)
+            {
+                string s = string.Format("{0,20} {1,30}", item.A, item.B);
+
+                Console.WriteLine(s);
+            }
+        }
+
+        /// <summary>
+        /// Displaying the official languages of each country.
+        /// </summary>
+        public static void OfficialLanguages()
+        {
+            string sql = "select concat(country.name, ' has ' + language.name + ' as their official language.') as [A] " +
+                "from language join langfam_lang_link on language.id = langfam_lang_link.lang_id " +
+                "join language_family on language_family.id = langfam_lang_link.langfam_id " +
+                "join country_lang_link on language.id = country_lang_link.lang_id " +
+                "join country on country.id = country_lang_link.country_id";
+            var q = db.Database.SqlQuery<Items<string>>(sql);
+
+            string header = string.Format("{0,20}", "Official language");
+
+            Console.WriteLine(header + "\n");
+
+            foreach (var item in q)
+            {
+                string s = string.Format("{0,20}", item.A);
+
+                Console.WriteLine(s);
+            }
+        }
+
+        /// <summary>
+        /// Number of languages by difficulty.
+        /// </summary>
+        public static void LanguagesByDifficulty()
+        {
+            string sql = "select language.difficulty as [A], count(language.id) as [B] " +
+                "from language group by language.difficulty " +
+                "order by [B] desc;";
+            var q = db.Database.SqlQuery<Items<string, int>>(sql);
+
+
+            string header = string.Format("{0,6} {1, 6}", "Difficulty", "No. of speakers");
+
+            Console.WriteLine(header + "\n");
+
+            foreach (var item in q)
+            {
+                string s = string.Format("{0,6} {1, 6}", item.A, item.B);
+
+                Console.WriteLine(s);
+            }
+        }
+
+        /// <summary>
+        /// Displays each of the languages and the number of their speakers in descending order.
+        /// </summary>
+        public static void NumberOfSpeakers()
+        {
+            string sql = "select language.name as [A], sum(country.population) as [B] " +
+                "from language " +
+                "join langfam_lang_link on language.id = langfam_lang_link.lang_id " +
+                "join language_family on language_family.id = langfam_lang_link.langfam_id " +
+                "join country_lang_link on language.id = country_lang_link.lang_id " +
+                "join country on country.id = country_lang_link.country_id " +
+                "group by language.name " +
+                "order by sum(country.population) desc;";
+            var q = db.Database.SqlQuery<Items<string, int>>(sql);
+
+            string header = string.Format("{0,20} {1, 20}", "Language", "Speakers");
+
+            Console.WriteLine(header + "\n");
+
+            foreach (var item in q)
+            {
+                string s = string.Format("{0,20} {1, 20}", item.A, item.B);
+
+                Console.WriteLine(s);
+            }
+        }
+
         private class Items<T1>
         {
             public T1 A { get; set; }
@@ -39,18 +171,6 @@ namespace Languages.Logic
             public T3 C { get; set; }
 
             public T4 D { get; set; }
-        }
-
-        static DatabaseEntities db = new DatabaseEntities();
-
-        public static void NamesOfCountries()
-        {
-            var q = db.Database.SqlQuery<Items<string, int>>("select name as [A], population as [B] from country;");
-
-            foreach (var item in q)
-            {
-                Console.WriteLine(item.A);
-            }
         }
     }
 }
--- CSHARP/OENIK_PROG3_2019_2_GO10DV/OENIK_PROG3_2019_2_GO10DV/Program.cs
+++ CSHARP/OENIK_PROG3_2019_2_GO10DV/OENIK_PROG3_2019_2_GO10DV/Program.cs
@@ -7,7 +7,10 @@ using Languages.Logic;
 
 namespace OENIK_PROG3_2019_2_GO10DV
 {
-    public class Program // Main program
+    /// <summary>
+    /// The main program.
+    /// </summary>
+    public class Program
     {
         private static void Main(string[] args)
         {
@@ -22,6 +25,7 @@ namespace OENIK_PROG3_2019_2_GO10DV
             Console.WriteLine("3. Modify entity");
             Console.WriteLine("4. Remove entity");
             Console.WriteLine("5. Use the JAVA web endpoint");
+            Console.WriteLine("6. Exit");
             var input = Console.ReadKey(true).Key;
             Console.Clear();
 
@@ -29,7 +33,7 @@ namespace OENIK_PROG3_2019_2_GO10DV
             {
                 case ConsoleKey.D1:
                 case ConsoleKey.NumPad1:
-                    Queries.NamesOfCountries();
+                    QueryMenu();
                     break;
                 case ConsoleKey.D2:
                 case ConsoleKey.NumPad2:
@@ -43,16 +47,71 @@ namespace OENIK_PROG3_2019_2_GO10DV
                 case ConsoleKey.D5:
                 case ConsoleKey.NumPad5:
                     break;
-                //case ConsoleKey.D6:
-                //case ConsoleKey.NumPad6:
-                //    break;
+                case ConsoleKey.D6:
+                case ConsoleKey.NumPad6:
+                    Environment.Exit(0);
+                    break;
                 default:
-                    Console.WriteLine("Invalid key. Press any key to return to the main menu.");
+                    Invalid();
                     break;
             }
 
             Console.ReadKey(true);
             Menu();
+        }
+
+        private static void QueryMenu()
+        {
+            Console.Clear();
+
+            Console.WriteLine("1. Rolling up languages and their difficulties");
+            Console.WriteLine("2. Language and the language families they belong to");
+            Console.WriteLine("3. Official languages by country");
+            Console.WriteLine("4. Number of languages by difficulty");
+            Console.WriteLine("5. Number of speakers by language");
+            Console.WriteLine("6. Exit");
+
+            var input = Console.ReadKey(true).Key;
+            Console.Clear();
+
+            switch (input)
+            {
+                case ConsoleKey.D1:
+                case ConsoleKey.NumPad1:
+                    Queries.NumberOfSpeakersRollup();
+                    break;
+                case ConsoleKey.D2:
+                case ConsoleKey.NumPad2:
+                    Queries.LanguageFamilies();
+                    break;
+                case ConsoleKey.D3:
+                case ConsoleKey.NumPad3:
+                    Queries.OfficialLanguages();
+                    break;
+                case ConsoleKey.D4:
+                case ConsoleKey.NumPad4:
+                    Queries.LanguagesByDifficulty();
+                    break;
+                case ConsoleKey.D5:
+                case ConsoleKey.NumPad5:
+                    Queries.NumberOfSpeakers();
+                    break;
+                case ConsoleKey.D6:
+                case ConsoleKey.NumPad6:
+                    Environment.Exit(0);
+                    break;
+                default:
+                    Invalid();
+                    break;
+            }
+
+            Console.ReadKey(true);
+            Menu();
+        }
+
+        private static void Invalid()
+        {
+            Console.WriteLine("Invalid key. Press any key to return to the main menu.");
         }
     }
 }
